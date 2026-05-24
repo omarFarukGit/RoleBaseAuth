f27e7df8ffefa4d8e37f3078a6550560d2dde769
omarFarukGit <muhammadabuhafz@gmail.com>
2026-05-24 12:07:56 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+const app: Application = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+export default app;
---
+++ src/config/dotenv.config.ts
@@ -0,0 +1,6 @@
+import "dotenv/config";
+
+const config = {
+  port: process.env.PORT,
+};
+export default config;
