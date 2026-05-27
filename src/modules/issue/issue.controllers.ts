87b5fc0e996b1dd665a39dd25d47faf9f367c363
omarFarukGit <muhammadabuhafz@gmail.com>
2026-05-27 11:19:57 +0600

    created issues funtionality

--- src/modules/issue/issue.controllers.ts
+++ src/modules/issue/issue.controllers.ts
@@ -1,18 +1,20 @@
 import type { Request, Response } from "express";
 import { IssueService } from "./issue.service";
 
-const createIsuue = async (req:Request,res:Response) => {
-
-    try {
-        const result=await IssueService.issueIntroDB()
-        
-    } catch (error:any) {
-        res.status(500).json({
-            success:true,
-            mesaage:error.message
-        })
-    }
-    
+const createIsuue = async (req: Request, res: Response) => {
+  try {
+    const result = await IssueService.issueIntroDB(req.body);
+    res.status(201).json({
+      success: true,
+      mesaage: "issues crated succesfully",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: true,
+      mesaage: error.message,
+    });
+  }
 };
 
 export const IssueController = {
--- src/modules/issue/issue.service.ts
+++ src/modules/issue/issue.service.ts
@@ -1,4 +1,17 @@
-const issueIntroDB = async () => {};
+import { pool } from "../../db/db";
+
+const issueIntroDB = async (payload: any) => {
+  const { id, title, description, status } = payload;
+  const result = await pool.query(
+    `
+        INSERT INTO issues(
+        id,title,description,status
+        ) VALUES($1,$2,$3,$4)
+        `,
+    [id, title, description, status],
+  );
+  return result.rows[0];
+};
 
 export const IssueService = {
   issueIntroDB,
